d34e4f3aac094ae18816ec966bb6e0bf7ea43123
Lukas <lukasdrinkwater97@gmail.com>
2024-09-19 10:21:24 +0100

    initial commit, base structure, calling AddManufacturerForm

---
+++ frontend/src/components/pages/AddManufacturerPage.jsx
@@ -0,0 +1,9 @@
+import AddManufacturerForm from "../forms/AddManufacturerForm";
+
+export default function AddManufacturerPage() {
+  return (
+    <>
+      <AddManufacturerForm />
+    </>
+  );
+}

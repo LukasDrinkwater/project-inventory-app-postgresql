7dadd568f23d868419bc2f78693bf1921f6dc7a1
Lukas <lukasdrinkwater97@gmail.com>
2024-09-19 10:23:09 +0100

    initial commit, base structure, import route components and get up catch all url route, set axios defaults

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import axios from "axios";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Extra react imports for Routes
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Set some default axios properties
+axios.defaults.baseURL =
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+axios.defaults.headers.common["Content-Type"] = "application/json";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        {/* /* is a catch all route */}
+        <Route path="/*" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
